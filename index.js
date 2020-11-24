3f62086553d1a8d01d3d4dbba1d090fd3374b408
Oscar Andrés Granada <oscar.granada@gorillalogic.com>
2020-11-24 10:34:50 -0500

    Updates to allow promises, changes on ode to use const intead of var

--- index.js
+++ index.js
@@ -1,71 +1,96 @@
-var request = require("request");
-var methods = require("./api-methods");
+const request = require('request');
+const methods = require('./api-methods');
 
 function serializeObjToUri(obj) {
   return Object.keys(obj)
     .map(function(key) {
-      return key + "=" + encodeURIComponent(obj[key]);
+      return key + '=' + encodeURIComponent(obj[key]);
     })
-    .join("&");
+    .join('&');
 }
 
-var Api = function(config) {
-  config = config || {}
-  const basicAuthToken = config.basicAuth ?
-    config.basicAuth.username + ':' + config.basicAuth.password + '@' : ''
-
-  this._uri = (config.protocol || 'http') + '://' + basicAuthToken +
-    (config.host || 'localhost') + ':' + (config.port || '12900') +
-    (config.path || '')
+function Api(config={}) {
+  const basicAuthToken = config.basicAuth
+    ? config.basicAuth.username + ':' + config.basicAuth.password + '@'
+    : '';
+  const protocol = config.protocol || 'http';
+  const host = config.host || 'localhost';
+  const port = config.port || '9000';
+  const path = config.path || '/api';
+  this._uri = `${protocol}://${basicAuthToken}${host}:${port}${path}`
 };
 
 Object.keys(methods).forEach(function(mName) {
-  var m = methods[mName];
+  const m = methods[mName];
 
   Api.prototype[mName] = function(parameters, path, callback) {
-    if (arguments.length === 1) callback = parameters;
-    if (arguments.length === 2) callback = path;
+    if (arguments.length === 1 && parameters.apply) callback = parameters;
+    if (arguments.length === 2 && path.apply) callback = path;
 
-    var computedPath = m.path;
-    if (typeof arguments[1] === "object") {
+    const computedPath = m.path;
+    if (typeof arguments[1] === 'object') {
       computedPath = m.path.replace(/{([^}]*)}/g, function(s, p) {
         return path[p];
       });
     }
 
-    var reqUri = this._uri + computedPath;
+    let reqUri = this._uri + computedPath;
 
-    if (m.method === "GET" && parameters) {
-      reqUri = reqUri + "?" + serializeObjToUri(parameters);
+    if (m.method === 'GET' && parameters) {
+      reqUri = reqUri + '?' + serializeObjToUri(parameters);
     }
 
-    var opts = {
+    const opts = {
       url: reqUri,
       method: m.method,
       headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: m.method !== "GET" && parameters ? parameters : null,
+      body: m.method !== 'GET' && parameters ? parameters : null,
       json: false
     };
-    request(opts, function(error, response, body) {
-      if (body === "") body = "{}";
-      if (error) {
-        return callback([error, body]);
-      }
-      if (response.statusCode === 403) {
-        return callback([JSON.parse(body).message, body]);
-      }
-
-      var parsedBody;
-      try {
-        parsedBody = JSON.parse(body);
-      } catch (err) {
-        callback(["Bad response", err, reqUri]);
-      }
-      
-      callback(null, parsedBody);
+    return new Promise((resolve, reject) => {
+      request(opts, function(error, response, body) {
+        if (body === '') body = '{}';
+        if (error) {
+          const resp = [error, body];
+          if (callback) {
+            reject(resp);
+            return callback(resp);
+          } else {
+            return reject(resp);
+          }
+        }
+        if (response.statusCode === 403) {
+          const resp = [JSON.parse(body).message, body];
+          if (callback) {
+            reject(resp);
+            return callback(resp);
+          } else {
+            return reject(resp);
+          }
+        }
+  
+        var parsedBody;
+        try {
+          parsedBody = JSON.parse(body);
+        } catch (err) {
+          const resp = ['Bad response', err, reqUri];
+          if (callback) {
+            reject(resp);
+            return callback(resp);
+          } else {
+            return reject(resp);
+          }
+        }
+        if (callback) {
+          resolve(parsedBody);
+          callback(null, parsedBody);
+        } else {
+          resolve(parsedBody);
+        }
+      });
     });
   };
 });
